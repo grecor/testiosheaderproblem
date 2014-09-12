7c036152b2ab0165dd857121ca8b925d33a33fbf
Ted Chang <tedchang77@gmail.com>
2014-09-12 02:46:27 -0400

    added code to modify header

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope,$cordovaStatusbar) {
+        $cordovaStatusbar.overlaysWebView(true);
+
+        // styles: Default : 0, LightContent: 1, BlackTranslucent: 2, BlackOpaque: 3
+        $cordovaStatusbar.style(1);
+
+        // supported names: black, darkGray, lightGray, white, gray, red, green,
+        // blue, cyan, yellow, magenta, orange, purple, brown
+        $cordovaStatusbar.styleColor('black');
+
+        $cordovaStatusbar.styleHex('#000');
+
+        $cordovaStatusbar.hide();
+
+        $cordovaStatusbar.show();
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
